e5c131b31f07a81cc47c61458f6965ae1b6bffa5
blue <playbynumbers@gmail.com>
2022-05-21 10:48:01 +0100

    add kinesis advantage map

---
+++ keyboards/kinesis/keymaps/theaquamarine/config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// place overrides here
+
+#define EE_HANDS
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+// #define TAPPING_FORCE_HOLD
+#undef TAPPING_FORCE_HOLD
+
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+// #define PERMISSIVE_HOLD
+#undef PERMISSIVE_HOLD
+
+// #undef PREVENT_STUCK_MODIFIERS
+
+#define COMBO_VARIABLE_LEN
+#define COMBO_TERM 35
